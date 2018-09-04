eba571c79a80664f9d228b2362562bcc9d083375
ssalyi <ssalyi@rdisoftware.com>
2018-09-04 18:37:00 +0200

    Promise version of the initial version

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const request = require('request');
 
+const rp = require('request-promise');
+
 let app = express();
 let bodyParser = require('body-parser');
 let url = 'https://api.punkapi.com/v2/beers';
@@ -10,26 +12,44 @@ let params = {
     'beer_name' : 'beer_name'
 };
 
+let options = {
+    uri: url,
+    json: true
+};
+
 app.use(bodyParser.json());
+
 app.get('/beers', (req,resp) => {
     let path = url;
     if(req.query.beer_name){
         path += `?${params.beer_name}=${req.query.beer_name}`;
+        options.uri = path;
     }
-    request(path, (error, response, body) => {
-        if(error){
-            resp.status(500).json(error);
-            return;
-        }
-        resp.json(JSON.parse(body));
+    rp(options).then(function (body) {
+        resp.send(body);
+    }).catch(function (err) {
+        resp.status(500).json(err);
     });
 });
 
+
+let postOptions = {
+    method: 'POST',
+    uri: 'https://httpbin.org/post',
+    header:{
+        'content-type': 'application/json'
+    },
+    json: true
+};
+
 app.post('/binpost', (req, resp) =>{
     console.log(req.body);
-    request.post('https://httpbin.org/post',{json:req.body}, (error, response, body) =>{
-      console.log(body);
-      resp.json(body);
+    options.body= {json : req.body};
+    console.log(JSON.stringify(options.body)+ "-----");
+    rp(postOptions).then(function (body) {
+        resp.json(body);
+    }).catch(function (err) {
+        resp.status(500).json(err);
     });
 });
 
